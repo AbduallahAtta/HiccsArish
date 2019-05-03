3b9656e543bdd9b6f9439794a364dba2c20bb618
AbduallahAtta <33333259+AbduallahAtta@users.noreply.github.com>
2019-05-03 22:44:30 +0200

    tested push

--- app/src/main/java/com/hiccs/arish/activities/AcademicMailActivity.java
+++ app/src/main/java/com/hiccs/arish/activities/AcademicMailActivity.java
@@ -3,6 +3,8 @@ package com.hiccs.arish.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hiccs.arish.R;
+
 public class AcademicMailActivity extends AppCompatActivity {
 
     @Override
